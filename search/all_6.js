631a036dd792f59ba310b687ca91ddc1fe98ce29
Jonathan Alberto <30415582+jonathanatss@users.noreply.github.com>
2019-09-23 02:15:07 -0300

    Add files via upload

---
+++ search/all_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['nc_10',['nc',['../class_sculptor.html#a645b884476c1463e997fa26d0e69f48f',1,'Sculptor']]],
+  ['nl_11',['nl',['../class_sculptor.html#a525224a4b6cfce4f04f5436840c55014',1,'Sculptor']]],
+  ['np_12',['np',['../class_sculptor.html#a0eda60149e22aee92e24e4a62f6247aa',1,'Sculptor']]]
+];
